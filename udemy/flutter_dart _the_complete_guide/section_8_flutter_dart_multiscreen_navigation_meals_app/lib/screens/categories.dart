0ef9a9704912e3883de51d41ce5a63b2969608b7
Dyegho <dyeghocunha@gmail.com>
2023-08-04 20:32:37 -0300

    (feat)

---
+++ udemy/flutter_dart _the_complete_guide/section_8_flutter_dart_multiscreen_navigation_meals_app/lib/screens/categories.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/data/dummy_data.dart';
+import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/models/category.dart';
+import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/screens/meals.dart';
+import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains
+    (category.id)).toList();
+
+
+    Navigator.push(context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pick you category"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          //availableCategories.map((category)=> CategoryGridItem(category:
+          // category))
+          for (final category in availableCategories)
+            CategoryGridItem(
+              onSelectCategory:(){ _selectCategory(context, category);},
+              category: category,
+            ),
+        ],
+      ),
+    );
+  }
+}
