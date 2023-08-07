d7f637aa547fd467448c5933b7e8c2f8726d2869
Dyegho <dyeghocunha@gmail.com>
2023-08-07 20:28:09 -0300

    (feat)

--- udemy/flutter_dart _the_complete_guide/section_8_flutter_dart_multiscreen_navigation_meals_app/lib/screens/categories.dart
+++ udemy/flutter_dart _the_complete_guide/section_8_flutter_dart_multiscreen_navigation_meals_app/lib/screens/categories.dart
@@ -5,18 +5,41 @@ import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/models/m
 import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/screens/meals.dart';
 import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key,  required this.availableMeals});
+class CategoriesScreen extends StatefulWidget {
+  const CategoriesScreen({super.key, required this.availableMeals});
 
-   final List<Meal> availableMeals;
+  final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals.where((meal) => meal.categories.contains
-    (category.id)).toList();
+    final filteredMeals = widget.availableMeals.where((meal) => meal.categories.contains(category.id)).toList();
 
-
-    Navigator.push(context,
+    Navigator.push(
+      context,
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
           title: category.title,
@@ -28,24 +51,35 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-      GridView(
-        padding: const EdgeInsets.all(24),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    return AnimatedBuilder(
+        animation: _animationController,
+        child: GridView(
+          padding: const EdgeInsets.all(24),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-        children: [
-          //availableCategories.map((category)=> CategoryGridItem(category:
-          // category))
-          for (final category in availableCategories)
-            CategoryGridItem(
-              onSelectCategory:(){ _selectCategory(context, category);},
-              category: category,
-            ),
-        ],
-      );
+            mainAxisSpacing: 20,
+          ),
+          children: [
+            //availableCategories.map((category)=> CategoryGridItem(category:category))
+            for (final category in availableCategories)
+              CategoryGridItem(
+                onSelectCategory: () {
+                  _selectCategory(context, category);
+                },
+                category: category,
+              ),
+          ],
+        ),
+        builder: (context, child) => SlideTransition(
+              position:
+                Tween(
+                  begin: const Offset(0, 0.3),
+                  end: const Offset(0, 0),
+                ).animate(CurvedAnimation(parent: _animationController, curve: Curves.easeInOut)),
 
+              child: child,
+            ));
   }
 }
