5700d310edef24c0c29b1cf76f6068ec8c060bbb
Dyegho <dyeghocunha@gmail.com>
2023-08-07 13:00:31 -0300

    (feat)

--- udemy/flutter_dart _the_complete_guide/section_8_flutter_dart_multiscreen_navigation_meals_app/lib/screens/categories.dart
+++ udemy/flutter_dart _the_complete_guide/section_8_flutter_dart_multiscreen_navigation_meals_app/lib/screens/categories.dart
@@ -6,12 +6,14 @@ import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/screens/
 import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key, required this.onToggleFavorite});
+  const CategoriesScreen({super.key, required this.onToggleFavorite, required this.availableMeals});
 
   final void Function(Meal meal) onToggleFavorite;
+   final List<Meal> availableMeals;
+
 
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains
+    final filteredMeals = availableMeals.where((meal) => meal.categories.contains
     (category.id)).toList();
 
 
--- udemy/flutter_dart _the_complete_guide/section_8_flutter_dart_multiscreen_navigation_meals_app/lib/screens/tabs.dart
+++ udemy/flutter_dart _the_complete_guide/section_8_flutter_dart_multiscreen_navigation_meals_app/lib/screens/tabs.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/data/dummy_data.dart';
 import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/models/meal.dart';
 import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/screens/categories.dart';
 import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/screens/filters.dart';
 import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/screens/meals.dart';
 import 'package:section_8_flutter_dart_multiscreen_navigation_meals_app/widgets/main_drawer.dart';
+
+const kInitialFilters = {
+  Filter.glutenFree:false,
+  Filter.lactoseFree:false,
+  Filter.vegetarian:false,
+  Filter.vegan:false,
+};
+
+
 
 class TabScreen extends StatefulWidget {
   const TabScreen({super.key});
@@ -14,22 +24,23 @@ class TabScreen extends StatefulWidget {
 
 class _TabScreenState extends State<TabScreen> {
   int _selectedPageIndex = 0;
+  Map<Filter,bool> _selectedFilters = kInitialFilters;
   final List<Meal> _favoriteMeals = [];
 
-  void _showInfoMessage(String message){
+  void _showInfoMessage(String message) {
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
   }
 
-  void _toggleMealFavoriteStatus(Meal meal){
+  void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
 
-    if (isExisting){
+    if (isExisting) {
       setState(() {
         _favoriteMeals.remove(meal);
       });
       _showInfoMessage("${meal.title} is no longer a favorite.");
-    }else{
+    } else {
       setState(() {
         _favoriteMeals.add(meal);
         _showInfoMessage("${meal.title} marked as favorite!");
@@ -37,44 +48,76 @@ class _TabScreenState extends State<TabScreen> {
     }
   }
 
- void _selectPage(int index){
-   setState(() {
-     _selectedPageIndex = index;
-   });
- }
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
 
- void _setScreen(String identifier){
-   Navigator.of(context).pop();
-    if(identifier =="filter") {
-      Navigator.of(context).push(MaterialPageRoute(builder: (context) => const FiltersScreen(),),);
+ void _setScreen(String identifier) async {
+    Navigator.of(context).pop();
+    if (identifier == "filter") {
+     final result = await Navigator.of(context).push<Map<Filter,bool>>(
+        MaterialPageRoute(
+          builder: (context) => FiltersScreen(currentFilters: _selectedFilters,),
+        ),
+      );
+     setState(() {
+       _selectedFilters = result ?? kInitialFilters;
+     });
+
     }
- }
+  }
 
   @override
   Widget build(BuildContext context) {
+    final availableMeals =  dummyMeals.where((meal){
+      if(_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree){
+        return false;
+      }
+      if(_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree){
+      return false;
+      }
+      if(_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian){
+        return false;
+      }
+      if(_selectedFilters[Filter.vegan]! && !meal.isVegan){
+        return false;
+      }
+      return true;
+    }).toList();
 
-   Widget activePage =  CategoriesScreen(onToggleFavorite:_toggleMealFavoriteStatus );
-   var activePageTitle= "Categories";
+    Widget activePage = CategoriesScreen(onToggleFavorite: _toggleMealFavoriteStatus, availableMeals: availableMeals,);
+    var activePageTitle = "Categories";
 
-   if(_selectedPageIndex ==1){
-     activePage =  MealsScreen( meals:_favoriteMeals, onToggleFavorite: _toggleMealFavoriteStatus,);
-     activePageTitle = "Your Favorites";
-   }
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(
+        meals: _favoriteMeals,
+        onToggleFavorite: _toggleMealFavoriteStatus,
+      );
+      activePageTitle = "Your Favorites";
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
       drawer: MainDrawer(onSelectScreeen: _setScreen),
-      body: activePage ,
+      body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         currentIndex: _selectedPageIndex,
         items: const [
-         BottomNavigationBarItem(icon: Icon(Icons.set_meal), label:"Categories", ),
-          BottomNavigationBarItem(icon:Icon(Icons.star) , label: "Favorites" ,),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: "Favorites",
+          ),
         ],
       ),
-    ) ;
+    );
   }
 }
