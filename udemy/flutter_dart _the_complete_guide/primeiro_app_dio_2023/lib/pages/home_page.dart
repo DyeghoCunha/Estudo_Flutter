5ad49569701a129aa814ae959b4d879f2db3e489
Dyegho <dyeghocunha@gmail.com>
2023-08-26 15:42:04 -0300

    feat

--- udemy/flutter_dart _the_complete_guide/primeiro_app_dio_2023/lib/pages/home_page.dart
+++ udemy/flutter_dart _the_complete_guide/primeiro_app_dio_2023/lib/pages/home_page.dart
@@ -1,109 +1,67 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:primeiro_app_dio_2023/pages/dados_cadastrais.dart';
 
-import '../service/gerador_numero_aleatorio.dart';
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  var numeroGerado = 0;
-  var quantidadeCliques = 0;
-
+class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Meu App",
-          //  style: GoogleFonts.pacifico(),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Simples Nacional"),
         ),
-      ),
-      body: Container(
-          width: double.infinity,
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  color: Colors.cyan,
-                  child: Text(
-                    "Ações do usuário",
-                    style: GoogleFonts.acme(),
+        drawer: Drawer(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(50),
+              bottomRight: Radius.circular(50),
+            ),
+          ),
+          child: Container(
+            decoration: BoxDecoration(image: DecorationImage(image: AssetImage
+              ("assets/images/fundoDrawer.png"),fit: BoxFit.cover)),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 40,horizontal: 10),
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Container(
+                    width: double.infinity,
+                    child: InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (ctx) => DadosCadastrais()));
+                        },
+                        child: Text("Dados Cadastrais"))),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("Termo de uso e privacidade"),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("Configurações"),
+                SizedBox(
+                  height: 10,
+                ),
+                Divider(),
+                ElevatedButton(onPressed: () {}, child: Text("LogOff"))
+              ]),
+            ),
+          ),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                    "assets/images/fundo4.png",
                   ),
-                ),
-              ),
-              SizedBox(
-                width: 100,
-                height: 100,
-                child: Container(
-                  color: Colors.orange,
-                  child: Text(
-                    "Foi clicado $quantidadeCliques vezes",
-                  ),
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                color: Colors.indigo,
-                child: Text(
-                  "O numero gerado foi: $numeroGerado",
-                  style: GoogleFonts.acme(),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.grey,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Container(
-                          alignment: Alignment.center,
-                          color: Colors.red,
-                          child: Text("10"),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 3,
-                        child: Container(
-                          alignment: Alignment.center,
-                          color: Colors.blue,
-                          child: Text("Dyegho Cunha"),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Container(
-                          alignment: Alignment.center,
-                          color: Colors.green,
-                          child: Text("30"),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            ],
-          )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            quantidadeCliques = quantidadeCliques + 1;
-            numeroGerado = GeradorNumeroAleatorioService.gerarNumeroAleatorio(10);
-          });
-        },
-        child: Icon(Icons.add),
+                  fit: BoxFit.cover)),
+        ),
       ),
     );
   }
